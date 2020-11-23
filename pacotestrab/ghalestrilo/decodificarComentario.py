55051cc42184d4b9579fd61cf0f0b876fc42793c
ghalestrilo <ghalestrilo@gmail.com>
2020-11-23 09:49:17 -0300

    processar comentário

--- pacotestrab/ghalestrilo/decodificarComentario.py
+++ pacotestrab/ghalestrilo/decodificarComentario.py
@@ -4,32 +4,44 @@ import os
 import sys
 import time
 
-video_id = ""
-
-def parsecontent(filename):
+def parsecomment(filename):
     # json.dumps
     with open(filename) as input_file:
         content = json.load(input_file)
     input_file.close()
     return content
 
-
-
-def write(content,filename):
+def create_parents(filename):
     if os.sep in filename:
         outdir = os.path.dirname(filename)
         if not os.path.exists(outdir):
             os.makedirs(outdir)
-    with open(filename) as output:
-        output.write(f'http://www.youtube.com/watch?v={video_id}')
-    output.close()
+
+def process_comment(comment_data):
+    return
+
+def process_comments(video_id):
+    file_in = f'dataset/{video_id}.json'
+    file_out = f'dataset/{video_id}.txt'
+    create_parents(file_out)
+
+    # with (open(file_in), open(file_out, "w")) as (input_file, output_file):
+    with open(file_in) as input_file:
+        lines = [];
+        line = input_file.readline()
+        print("")
+        while line:
+            data = json.loads(line);
+            line = f'{data["author"]}\n{data["text"]}\n{data["time"]}\n'
+            print(line)
+            line = input_file.readline()
+            
+    input_file.close()
+    # output_file.close()
 
 def main(argv):
     video_id = argv[0];
-    file_in = f'dataset/{video_id}.json'
-    file_out = f'dataset/{video_id}.txt'
-    content = parsecontent(file_in)
-    write(content,file_out)
+    process_comments(video_id)
     return
 
 if __name__ == "__main__":
